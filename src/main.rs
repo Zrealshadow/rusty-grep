1e44f935789816d61e2aa948231e6fad2c588858
Zreal <zenglz_pro@163.com>
2021-09-27 21:54:42 +0800

    first commit

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+mod lib;
+
+use std::env;
+use std::process;
+use lib::{Config, run};
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {}", err);
+        process::exit(1)
+    });
+    if let Err(e) = run(config){
+        eprintln!("Application Error : {}", e);
+        process::exit(1);
+    }
+}
